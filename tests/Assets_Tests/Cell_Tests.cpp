12117e143e940bb78b93bf8d78bd267b284fb926
PhilTaylor <Phil@DESKTOP-CIL6N53>
2023-11-27 22:12:05 +0000

    Removed redundant setPosition test from Cell_Tests.cpp

--- tests/Assets_Tests/Cell_Tests.cpp
+++ tests/Assets_Tests/Cell_Tests.cpp
@@ -3,23 +3,6 @@
 
 namespace Cell_Tests
 {
-	//TEST(Cell_setPosition_Inherited_Test, Should_set_position_to_specified_values)
-	//{
-	//	Cell testCell_1 = Cell();
-	//	testCell_1.setPosition(10, 10);
-	//	sf::Vector2f expectedPosition_1 = sf::Vector2f(10, 10);
-	//	ASSERT_EQ(testCell_1.getPosition(), expectedPosition_1);
-
-	//	Cell testCell_2 = Cell();
-	//	testCell_2.setPosition(30.5, 5.7);
-	//	sf::Vector2f expectedPosition_2 = sf::Vector2f(30.5, 5.7);
-	//	ASSERT_EQ(testCell_2.getPosition(), expectedPosition_2);
-
-	//	testCell_2.setPosition(10, 10);
-	//	sf::Vector2f expectedPosition_3 = sf::Vector2f(10, 10);
-	//	ASSERT_EQ(testCell_2.getPosition(), expectedPosition_3);
-	//}
-
 	TEST(Cell_Constructor_Inherited_Test, Should_initailise_position_to_zero_value)
 	{
 		Cell testCell = Cell();
