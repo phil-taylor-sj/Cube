2e0282021acf9bd8adf02bd8d8f631ab1e406a06
PhilTaylor <Phil@DESKTOP-CIL6N53>
2024-02-17 22:22:43 +0000

    Updated the ReadMe file.

--- test/Legacy_Test/Assets_Test.cmake
+++
@@ -1,9 +0,0 @@
-set(ASSETS_TEST_DIR Assets_Test)
-
-set(ASSETS_TEST
-    ${ASSETS_TEST_DIR}/Cell_Test.cpp
-    ${ASSETS_TEST_DIR}/Character_Test.cpp
-    ${ASSETS_TEST_DIR}/Player_Test.cpp
-    ${ASSETS_TEST_DIR}/Room_Test.cpp
-    ${ASSETS_TEST_DIR}/SpriteAsset_Test.cpp
-)
--- test/Levels_Test/LevelEntityManager_Test.cpp
+++ test/Levels_Test/LevelEntityManager_Test.cpp
@@ -3,21 +3,73 @@
 #include "Actors/ActorComponents.h"
 #include "Physics/Physics.h"
 
+
+
 namespace LevelEntityManager_Tests
 {
-	TEST(Level_getCollision_test,
-		Should_return_non_empty_vector_for_actor_overlapping_walls)
+	class SmallGridFixture: public ::testing::Test 
 	{
-		Levels::LevelEntityManager level = Levels::LevelEntityManager(5, 5);
-		level.setCommonCellWidth(1000.f);
-		level.updateAllCellScaling();
-		
+	protected:
+		Levels::LevelEntityManager level;
 		Actors::ActorCollisionComponent actorOne;
-		actorOne.broadCircle.setPosition(1000.f, 1000.f);
-		actorOne.broadCircle.setRadius(50.f);
-		std::vector<Physics::RectParams>collisionsOne = 
-			level.getCircleCollisions(actorOne);
+		void SetUp() override {
+			level = Levels::LevelEntityManager(5, 5);
+			level.setCommonCellWidth(1000.f);
+			level.updateAllCellScaling();
+			actorOne.broadCircle.setRadius(50.f);
+			actorOne.broadCircle.setPosition(1000.f, 1000.f);
+		}
+		void TearDown() override {}
+	};
 
-		EXPECT_EQ(collisionsOne.size(), 8);
+	class DefaultGridFixture : public ::testing::Test
+	{
+	protected:
+		Levels::LevelEntityManager level;
+		Actors::ActorCollisionComponent actorOne;
+		void SetUp() override {
+			level = Levels::LevelEntityManager(5, 5);
+			actorOne.broadCircle.setRadius(50.f);
+			actorOne.broadCircle.setPosition(0.f, 0.f);
+		}
+		void TearDown() override {}
+	};
+
+	TEST_F(DefaultGridFixture,
+		setCommonCellWidth_should_set_new_cell_width_and_scale_cells
+		)
+	{
+		level.setCommonCellWidth(2000.f);
+
+		Levels::DetectedLevelCollisions collisionsOne =
+			level.getCircleCollisions(actorOne);
+		EXPECT_EQ(collisionsOne.staticWalls.size(), 0);
+	
+		actorOne.broadCircle.translate(Physics::Vec2f(1000.f, 1000.f));
+		Levels::DetectedLevelCollisions collisionsTwo =
+			level.getCircleCollisions(actorOne);
+		EXPECT_EQ(collisionsTwo.staticWalls.size(), 8);
+	}
+
+	TEST_F(SmallGridFixture,
+		getCircleCollisions_should_return_non_empty_wall_vector_for_actor_overlapping_walls)
+	{
+		Levels::DetectedLevelCollisions collisionsOne = 
+			level.getCircleCollisions(actorOne);
+		EXPECT_EQ(collisionsOne.staticWalls.size(), 8);	
+		
+		actorOne.broadCircle.translate(Physics::Vec2f(250.f, 0.f));
+		Levels::DetectedLevelCollisions collisionsTwo =
+			level.getCircleCollisions(actorOne);
+		EXPECT_EQ(collisionsTwo.staticWalls.size(), 2);
+	}
+
+	TEST_F(SmallGridFixture,
+		getCircleCollisions_should_return_empty_wall_vector_for_actor_not_overlapping_walls)
+	{
+		Levels::DetectedLevelCollisions collisionsOne =
+			level.getCircleCollisions(actorOne);
+		actorOne.broadCircle.setPosition(500.f, 500.f);
+		EXPECT_EQ(collisionsOne.staticWalls.size(), 0);
 	}
 }
